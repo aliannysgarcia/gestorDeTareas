e9270b13202c8fddfb10a3e26a863464ee2d9295
Virginia <ali.a210396@gmail.com>
2024-01-23 13:02:47 -0300

    agregue la base de datos con las tareas

---
+++ src/data/tareas.js
@@ -0,0 +1,18 @@
+const tareas = [
+    {
+        id: 1,
+        fecha: new Date(),
+        tarea: 'Lavar los platos'
+    },
+    {
+        id: 2,
+        fecha: new Date(),
+        tarea: 'Salir a pasear'
+    },
+    {
+        id: 3,
+        fecha: new Date(),
+        tarea: 'Correr'
+    }
+]
+export default tareas
