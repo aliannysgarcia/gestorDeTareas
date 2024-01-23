57386fe38f5e1708fbf14688f5e922a62bfb80e8
Virginia <ali.a210396@gmail.com>
2024-01-23 12:59:09 -0300

    agregue la funcion agregarTarea y eliminarTarea usando swwetalert2

--- src/components/GestionarTareas.jsx
+++ src/components/GestionarTareas.jsx
@@ -1,16 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Formulario from './Formulario'
 import Tabla from './Tabla'
-
+import BaseDeDatos from '../data/tareas'
+import Swal from 'sweetalert2'
 
 const GestionarTareas = () => {
-  
-  
+
+  const [tareas, setTareas] = useState(BaseDeDatos)
+
+  const agregarTarea = (nuevaTarea) => {
+
+    const tareaConFecha = {
+      ...nuevaTarea,
+      id: Date.now(),
+      fecha: new Date(),
+    };
+
+    const nuevoEstado = [...tareas, tareaConFecha]
+    setTareas(nuevoEstado)
+  }
+  const eliminarTarea = id => {
+    Swal.fire({
+      title: "¿Estás seguro que quieres borrar esta tarea?",
+      text: "No podrás volver atrás",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Eliminar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const nuevoEstado = tareas.filter(tarea => tarea.id !== id)
+        // console.log(nuevoEstado)
+        setTareas(nuevoEstado)
+
+        Swal.fire({
+          title: "Tarea eliminada!",
+          text: "La tarea fue borrada.",
+          icon: "success"
+        });
+      } else {
+        Swal.fire({
+          title: "No lo borró!",
+          text: "La tarea no fue borrada.",
+          icon: "success"
+      })}
+    });
+
+    const nuevoEstado = tareas.filter(tarea => tarea.id !== id)
+    // console.log(nuevoEstado)
+    setTareas(nuevoEstado)
+
+  }
 
   return (
     <div className='container mt-5'>
-    <Formulario />
-    <Tabla />
+      <Formulario agregarTarea={agregarTarea} />
+      <Tabla tareas={tareas} eliminarTarea={eliminarTarea} />
     </div>
   )
 }
