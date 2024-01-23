b6b6a17a5486be5b267ef23def53e00a005c7444
Virginia <ali.a210396@gmail.com>
2024-01-23 12:57:01 -0300

    agregue los handle para manejar y enviar cambios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Formulario = () => {
+const Formulario = ({agregarTarea}) => {
+
+    const formInicial = {
+        fecha: '',
+        tarea: ''
+    }
+
+    const [form, setForm] = useState(formInicial)
+
+    const handleChange = e => { // para manejar cambios en el form
+        console.log(e.target.name)
+        console.log(e.target.value)
+
+        const datoIngresado = {
+            ...form,
+            [e.target.name]: e.target.value
+        }
+        
+        setForm(datoIngresado)
+    }
+    const handleSubmit = e => { //maneja el envio del formulario
+        e.preventDefault() //evita el comportamiento predeterminado
+        console.log('se presiono el boton')
+        agregarTarea(form)
+        setForm(formInicial);
+    }
 
     return (
-        <form className="row g-3 d-flex justify-content-center">
+        <form className="row g-3 d-flex justify-content-center" onSubmit={handleSubmit}>
             <div className="col-auto">
                 <label htmlFor="tarea" className="visually-hidden">Tarea</label>
-                <input type="text" className="form-control" id="tarea" placeholder="Ingrese la Tarea"/>
+                <input type="text" className="form-control" id="tarea" name="tarea" onChange={handleChange} value={form.tarea} placeholder="Ingrese la Tarea" />
             </div>
             <div className="col-auto">
                 <button type="submit" className="btn btn-success mb-2">Agregar Tarea</button>
