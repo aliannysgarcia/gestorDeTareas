fd41017f27aa4208f186bb516d1e37ed68629119
Virginia <ali.a210396@gmail.com>
2024-01-23 13:01:12 -0300

    creacion del handleEliminar

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const Tareas = ({tarea}) => {
+const Tareas = ({tarea, eliminarTarea}) => {
+
+    const handleEliminar = id => {
+        console.log('se presiono el boton eliminar con el id:', id)
+        eliminarTarea(id)
+    }
 
     return (
         <tr>
-            <th scope="row">{tarea.fecha}</th>
+            <th scope="row">{tarea.fecha ? tarea.fecha.toLocaleString() : ''}</th>
             <td>{tarea.tarea}</td>
             <td>
                 <input className="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input"/>
             </td>
             <td>
                 <button className='btn btn-warning me-1'>Editar</button>
-                <button className='btn btn-danger'>Eliminar</button>
+                <button className='btn btn-danger' onClick={()=>handleEliminar(tarea.id)}>Eliminar</button>
             </td>
         </tr>
         
