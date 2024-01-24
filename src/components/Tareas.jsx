228141cf582a6431932c79de1337c7490d2b0b98
Virginia <ali.a210396@gmail.com>
2024-01-24 19:08:40 -0300

    se agrego el handleEditar

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Tareas = ({tarea, eliminarTarea}) => {
+const Tareas = ({tarea, eliminarTarea, setTareaAEditar}) => {
 
     const handleEliminar = id => {
         console.log('se presiono el boton eliminar con el id:', id)
         eliminarTarea(id)
+    }
+    const handleEditar = tarea => {
+        console.log(tarea)
+        setTareaAEditar(tarea)
     }
 
     return (
@@ -15,7 +19,7 @@ const Tareas = ({tarea, eliminarTarea}) => {
                 <input className="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input"/>
             </td>
             <td>
-                <button className='btn btn-warning me-1'>Editar</button>
+                <button className='btn btn-warning me-1'onClick={()=>handleEditar(tarea)}>Editar</button>
                 <button className='btn btn-danger' onClick={()=>handleEliminar(tarea.id)}>Eliminar</button>
             </td>
         </tr>
