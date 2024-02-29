93b1ee6af428a30d9917e9f184ecba1afa21cf0f
Virginia <ali.a210396@gmail.com>
2024-02-29 17:29:24 -0300

    modificaciones en agregarTarea y en eliminarTarea

--- src/components/GestionarTareas.jsx
+++ src/components/GestionarTareas.jsx
@@ -1,65 +1,117 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Formulario from './Formulario'
 import Tabla from './Tabla'
-import BaseDeDatos from '../data/tareas'
 import Swal from 'sweetalert2'
+import moment from 'moment'
 
 const GestionarTareas = () => {
 
-  const [tareas, setTareas] = useState(BaseDeDatos)
+  const [tareas, setTareas] = useState(null)
   const [tareaAEditar, setTareaAEditar] = useState(null)
+  const url = 'http://localhost:8080/tareas/'
 
-  const agregarTarea = (nuevaTarea) => {
+  useEffect(() => {
+    obtenerTodasLasTareas()
+  }, [])
 
-    const tareaConFecha = {
-      ...nuevaTarea,
-      id: Date.now(),
-      fecha: new Date(),
-    };
+  const obtenerTodasLasTareas = async () => {
+    try {
+      const res = await fetch(url)
+      if (!res.ok) {
+        throw new Error('Hubo un error', res.status, res.statusText)
+      }
+      const productosObtenidos = await res.json()
+      console.log(productosObtenidos)
+      setTareas(productosObtenidos)
 
-    const nuevoEstado = [...tareas, tareaConFecha]
-    setTareas(nuevoEstado)
+    } catch (error) {
+      console.log('[obtenerTodasLasTareas]', error)
+    }
   }
-  const eliminarTarea = id => {
-    Swal.fire({
-      title: "¿Estás seguro que quieres borrar esta tarea?",
-      text: "No podrás volver atrás",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Eliminar"
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const nuevoEstado = tareas.filter(tarea => tarea.id !== id)
-        // console.log(nuevoEstado)
-        setTareas(nuevoEstado)
 
-        Swal.fire({
-          title: "Tarea eliminada!",
-          text: "La tarea fue borrada.",
-          icon: "success"
-        });
-      } else {
-        Swal.fire({
-          title: "No lo borró!",
-          text: "La tarea no fue borrada.",
-          icon: "success"
-      })}
-    });
+  const agregarTarea = async (nuevaTarea) => {
+    try {
+      const fechaHora = moment().format('DD-MM-YYYY HH:mm');
+      const nuevaTareaConFecha = {
+        ...nuevaTarea,
+        id: Date.now().toString(),
+        fecha: fechaHora
+      };
+
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(nuevaTareaConFecha)
+      }
+
+      const res = await fetch(url, options)
+      if (!res.ok) {
+        throw new Error('Hubo un error', res.status, res.statusText)
+      }
+      const tareaAgregada = await res.json()
+      console.log(tareaAgregada)
+
+      const nuevoEstado = [...tareas, tareaAgregada]
+      setTareas(nuevoEstado)
 
 
+    } catch (error) {
+      console.log('[agregarTarea]', error)
+    }
   }
+
+  const eliminarTarea = (id) => {
+    try {
+      Swal.fire({
+        title: "¿Estás seguro que quieres borrar esta tarea?",
+        text: "No podrás volver atrás",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Eliminar"
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const options = {
+            method: 'DELETE'
+          }
+          const urlDelete = url + id;
+            console.log("URL para eliminar: ", urlDelete); // http://localhost:8080/productos/id
+
+          const res = await fetch(urlDelete, options)
+          if (!res.ok) {
+            throw new Error(`No se pudo borrar el producto ${res.status} ${res.statusText}`)
+          }
+          const tareaEliminada = await res.json() // { }
+
+          // console.log(tareaEliminada)
+          const nuevoEstadoTarea = tareas.filter(tarea => tarea.id !== id)
+          // console.log(nuevoEstado)
+          setTareas(nuevoEstadoTarea)
+
+          Swal.fire({
+            title: "Tarea eliminada!",
+            text: "La tarea se eliminó correctamente.",
+            icon: "success"
+          });
+        }
+      });
+    } catch (error) {
+      console.log('[eliminarTarea]', error)
+    }
+  }
+
   const editarTarea = (tareaAEditar) => {
+    console.log('editandoooooo')
     const nuevoEstadoTarea = tareas.map(tarea => tarea.id === tareaAEditar.id ? tareaAEditar : tarea)
     setTareas(nuevoEstadoTarea)
   }
 
   return (
-    <div className='container mt-5'>
-      <Formulario agregarTarea={agregarTarea} tareaAEditar={tareaAEditar} editarTarea={editarTarea}/>
-      <Tabla tareas={tareas} eliminarTarea={eliminarTarea} setTareaAEditar={setTareaAEditar}/>
-    </div>
+    <>
+      <Formulario agregarTarea={agregarTarea} tareaAEditar={tareaAEditar} editarTarea={editarTarea} />
+      <Tabla tareas={tareas} eliminarTarea={eliminarTarea} setTareaAEditar={setTareaAEditar} />
+    </>
   )
 }
 
